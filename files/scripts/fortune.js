ebedf06590f39d667f682e9b13953c230ddcafeb
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2022-04-30 13:50:37 -0300

    docs: Added Creative Commons licence formally.

--- files/scripts/fortune.js
+++ files/scripts/fortune.js
@@ -3,7 +3,8 @@ function fortune6(){
 		"dixitque Deus <b>fiat lux</b> et facta est lux",
 		"Sapere aude",
 		"Errare humanum est, perseverare autem diabolicum",
-		"Verum sine mendacio, certum et verissimum"
+		"Verum sine mendacio, certum et verissimum",
+		"<b>Pray</b> as though everything depended on God;<br /><b>act</b> as though everything depended on you"
 	];
 	var randomfortune = fortune[Math.floor(Math.random() * fortune.length)];
 	var chtxt = document.getElementById('fortune');
